8dd844effd2205fe9ea4cdc11d142ef34ab8b44c
Kevin Tam <tam.kevin@hotmail.com>
2017-08-09 17:10:43 -0700

    first commit

---
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,13 @@
+package org.launchcode.cheesemvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CheeseController {
+
+    @RequestMapping(value = "")
+    public String index(){
+        return "index";
+    }
+}
