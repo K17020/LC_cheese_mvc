fd3138d6f6ba18423dae9a286feceeb22a3a87bf
Kevin Tam <tam.kevin@hotmail.com>
2017-08-09 17:16:54 -0700

    Move files into cheese folder and added request map of cheese

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+// Changes the request map to /cheese
+@RequestMapping("cheese")
 public class CheeseController {
 
     @RequestMapping(value = "")
     public String index(){
-        return "index";
+        return "cheese/index";
     }
 }
