255abc48935bbd46e83ecb34f74b86629f23c40f
Kevin Tam <tam.kevin@hotmail.com>
2017-08-10 14:31:12 -0700

    Setup Get and Post request to properly display array list contents to page

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -3,6 +3,8 @@ package org.launchcode.cheesemvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -11,22 +13,32 @@ import java.util.ArrayList;
 @RequestMapping("cheese")
 public class CheeseController {
 
+    static ArrayList<String> cheeses = new ArrayList<>();
+
+
     // request location is at the root
     @RequestMapping(value = "")
     public String index(Model model){
-
-        /** Location for the page template
-         * model attribute works like a key value pare
-         * we are using time leaf template engine
-        */
-        // Holds the names of the cheeses
-        ArrayList<String> cheeses = new ArrayList<>();
-        cheeses.add("Chedder");
-        cheeses.add("Swiss");
-        cheeses.add("Munster");
 
         model.addAttribute("cheeses", cheeses); // pass the array list to the view
         model.addAttribute("title","My Cheeses"); // passes data into the view
         return "cheese/index";
     }
+
+    // Form that get request
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAndCheeseForm(Model model){
+        model.addAttribute("title","Add Cheese");
+        return "cheese/add";
+    }
+
+    // Post request for the form
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    // Handle going to pass param named cheeseName
+    public String processAddCheeseForm(@RequestParam String cheeseName){
+        cheeses.add(cheeseName);
+        // redirect /cheese at the root
+        return "redirect:";
+    }
+
 }
