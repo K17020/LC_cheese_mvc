8be302f2064d06a38302ecdecec2da3613a8cb9b
Kevin Tam <tam.kevin@hotmail.com>
2017-08-30 13:06:26 -0700

    Add unique identifier when create new objects

--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -4,10 +4,19 @@ public class Cheese {
 
     private String name;
     private String description;
+    private int cheeseID; // A number to keep track of object
+    private static int nextID = 1; // This field is shared across objects
 
     public Cheese(String name, String description) {
+        this(); // Call the default constructor
         this.name = name;
         this.description = description;
+    }
+
+    // This is the default constructor
+    public Cheese(){
+        cheeseID = nextID; // Assign the first object the static value
+        nextID++; // Adds 1 to each object that is created after
     }
 
     public String getName() {
@@ -24,5 +33,20 @@ public class Cheese {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+    public int getCheeseID() {
+        return cheeseID;
+    }
+
+    public void setCheeseID(int cheeseID) {
+        this.cheeseID = cheeseID;
+    }
+
+    public static int getNextID() {
+        return nextID;
+    }
+
+    public static void setNextID(int nextID) {
+        Cheese.nextID = nextID;
     }
 }
