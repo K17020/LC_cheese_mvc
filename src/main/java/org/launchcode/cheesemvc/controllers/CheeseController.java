a6b38d9c8a5b4e0ba17713444d4f1c703fea55e2
Kevin Tam <tam.kevin@hotmail.com>
2017-08-30 14:22:14 -0700

    Refactor code to reflect changes to object model

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,62 +1,59 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
+/**
+ * Created by LaunchCode
+ */
 @Controller
-// Changes the request map to /cheese
 @RequestMapping("cheese")
 public class CheeseController {
 
-
-    static HashMap<String,String> cheeses = new HashMap<>();
-
-
-    // request location is at the root
+    // Request path: /cheese
     @RequestMapping(value = "")
-    public String index(Model model){
+    public String index(Model model) {
 
-        model.addAttribute("cheeses", cheeses); // pass the array list to the view
-        model.addAttribute("title","My Cheeses"); // passes data into the view
+        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("title", "My Cheeses");
+
         return "cheese/index";
     }
 
-    // Form that get request
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAndCheeseForm(Model model){
-        model.addAttribute("title","Add Cheese");
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
         return "cheese/add";
     }
 
-    // Post request for the form
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    // Handle going to pass param named cheeseName
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc){
-        cheeses.put(cheeseName,cheeseDesc);
-        // redirect /cheese at the root
+    public String processAddCheeseForm(@ModelAttribute Cheese newCheese) {
+        CheeseData.add(newCheese);
         return "redirect:";
     }
 
-    // Displays page of the cheeses you can remove
     @RequestMapping(value = "remove", method = RequestMethod.GET)
-    public String displayRemoveCheeseForm(Model model){
-        model.addAttribute("cheeses", cheeses); // pass the array list to the view
-        model.addAttribute("title","Remove Cheese"); // passes data into the view
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("title", "Remove Cheese");
         return "cheese/remove";
     }
-    //removes cheeses from an array
-    @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String removeCheeseForm(@RequestParam String[] cheeseRemove){
-        for (String cheese:cheeseRemove){
-            cheeses.remove(cheese);
 
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
+
+        for (int cheeseId : cheeseIds) {
+            CheeseData.remove(cheeseId);
         }
+
         return "redirect:";
     }
 
