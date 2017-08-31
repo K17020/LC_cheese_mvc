fdc96d3fe2d7cf5a591355253a2e689467edc149
Kevin Tam <tam.kevin@hotmail.com>
2017-08-31 15:58:50 -0700

    Display form for user to enter information

--- src/main/java/org/launchcode/cheesemvc/controllers/UserValidation.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserValidation.java
@@ -2,9 +2,25 @@ package org.launchcode.cheesemvc.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("user")
 public class UserValidation {
+
+
+    // Default mapping to the page
+    @RequestMapping(value="")
+    public String index(Model model){
+        return "user/index";
+    }
+
+    @RequestMapping(value="add")
+    public String Add(Model model){
+        model.addAttribute("title","Add User");
+        return "user/add";
+    }
+
 }
--- src/main/java/org/launchcode/cheesemvc/models/User.java
+++ src/main/java/org/launchcode/cheesemvc/models/User.java
@@ -1,4 +1,44 @@
 package org.launchcode.cheesemvc.models;
 
 public class User {
+
+    // Fields
+    private String userName;
+    private String email;
+    private String password;
+
+    // Default Constructor
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+    // Constructors
+
+
+    // Setter & Getters
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
