7bc767009586439f98e4fd666a60001c4878b8d9
Kevin Tam <tam.kevin@hotmail.com>
2017-09-14 13:56:33 -0700

    Added cheese to category

--- src/main/java/org/launchcode/cheesemvc/models/Category.java
+++ src/main/java/org/launchcode/cheesemvc/models/Category.java
@@ -1,14 +1,19 @@
 package org.launchcode.cheesemvc.models;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity // Maps the class to the database
 public class Category {
+    // Relationships
+    @OneToMany // There can be many types of cheeses, but can only have one category
+    @JoinColumn(name="category_id") // We are telling Hibernate that we are joining the columns on the category id
+    private List<Cheese> cheeses = new ArrayList<>();
+
     // Fields
 
     // This is the key in the database and it will auto increment
